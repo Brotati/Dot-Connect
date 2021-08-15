f839b941dca6cddcb988089e6e1c9ace4303633d
Brotati katha <brotatihalderkatha@gmail.com>
2021-08-15 17:13:56 +0600

    Login Functionality added

--- src/Components/feed/Feed.jsx
+++ src/Components/feed/Feed.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import "./feed.css";
 import Share from "../share/Share";
-import Post from "../post/Post";
+import {Posts} from "../../dummyData";
+import Post from "../post/Post"
 
 export default function Feed() {
     return (
         <div className="Feed">
            <div className="feedwrapper">
            <Share />
-           <Post />
-           <Post />
-           <Post />
-           <Post />
-           <Post />
+           {Posts.map((p) => (
+          <Post key={p.id} post={p} />
+        ))}
            </div>
         </div>
     )
--- src/Components/post/Post.jsx
+++ src/Components/post/Post.jsx
@@ -1,10 +1,17 @@
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
 //import { Users } from "../../dummyData";
-//import { useState } from "react";
+import React, { useState } from "react";
 
-export default function Post() {
+export default function Post({ post }) {
+    const [like,setLike] = useState(post.like)
+    const [isLiked,setIsLiked] = useState(false)
   
+    const likeHandler =()=>{
+      setLike(isLiked ? like-1 : like+1)
+      setIsLiked(!isLiked)
+    }
+   
   return (
     <div className="post">
       <div className="postWrapper">
@@ -30,9 +37,9 @@ export default function Post() {
         </div>
         <div className="postBottom">
           <div className="postBottomLeft">
-            <img className="likeIcon" src="assets/like1.png"  alt="" />
-            <img className="likeIcon" src="assets/heart.png"  alt="" />
-            <span className="postLikeCounter">32 people like it</span>
+            <img className="likeIcon" src="assets/like1.png" onClick={likeHandler} alt="" />
+            <img className="likeIcon" src="assets/heart.png"  onClick={likeHandler} alt="" />
+            <span className="postLikeCounter">{like} people like it</span>
           </div>
           <div className="postBottomRight">
             <span className="postCommentText">9 comments</span>
